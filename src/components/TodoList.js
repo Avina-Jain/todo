5577cebbfbe782e44c1d5a114d7be2d2792911fa
AV <av@microsoft.com>
2021-05-09 20:36:06 +0530

    staring with header

---
+++ src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <div class = "header">
+            <h3>My Tasks</h3>
+            <span>My Tasks</span>
+
+            <h3>Boards</h3>
+            <h3>Schedule</h3>
+            <h3>...</h3>
+
+            <h3>User</h3>
+            <h3>Filter</h3>
+            <h3>GroupbyPrecess</h3>
+
+        </div>
+    );
+
+}
+
+export default TodoList;
