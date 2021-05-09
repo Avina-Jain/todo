cdccddd49b69eb1ce0762f79f71c8151eb8c6b93
AV <av@microsoft.com>
2021-05-09 21:03:54 +0530

    adding tabs Boards and Schedule

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList'
+import Tabs from './components/Tabs'
 import './components/TodoList.css'
 
 function App() {
   return (
     <div className="App">
       <TodoList />
+      <Tabs />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 const TodoList = () => {
     return (
-        <div class = "header">
-            <h3>My Tasks</h3>
-            <span>My Tasks</span>
+        <>
+            <div className = "marginTop">
 
-            <h3>Boards</h3>
-            <h3>Schedule</h3>
-            <h3>...</h3>
+            </div>
+            <div className = "header">
+                <h3>My Tasks</h3>
+                <span>My Tasks</span>
 
-            <h3>User</h3>
-            <h3>Filter</h3>
-            <h3>GroupbyPrecess</h3>
+                <h3>User</h3>
+                <h3>Filter</h3>
+                <h3>GroupbyPrecess</h3>
 
-        </div>
+            </div>
+
+            <div className = "task-container">
+
+            </div>
+        </>
     );
 
 }
